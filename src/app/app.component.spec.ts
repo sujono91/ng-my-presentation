b6e8024a5fbc976aaa3b7fab2b267264b8cab47b
sujono91 <sujonochen91@gmail.com>
2019-06-13 19:21:45 +0800

    Update Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-my-presentation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-my-presentation');
+    expect(app.title).toEqual('ng-my-presentatio');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-my-presentation!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-my-presentatio!');
   });
 });
